6bd1751c747f7b321c2e3143bb5ad30e6fa1e11f
Alberto Basalo <albertobasalo@hotmail.com>
2024-09-09 19:16:50 +0200

    Refactor: Rename AdminController to AuxController
    - Updated aux.controller.ts with new class name and route
    - Modified authentication.e2e.spec.ts to use new 'aux' endpoint
    - Created aux.controller.spec.ts with unit tests for AuxController
    - Updated imports to use path aliases

--- src/api/authentication/authentication.module.ts
+++ src/api/authentication/authentication.module.ts
@@ -4,8 +4,8 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { SharedModule } from '@shared/shared.module';
-import { AdminController } from './admin/admin.controller';
 import { AuthenticationController } from './authentication.controller';
+import { AuxController } from './aux/aux.controller';
 import { AuthenticationService } from './services/authentication.service';
 import { HashService } from './services/hash.service';
 import { TokenService } from './services/token.service';
@@ -16,7 +16,7 @@ import { UserEntity } from './services/user.entity';
  */
 @Module({
   imports: [MikroOrmModule.forFeature([UserEntity]), JwtModule.register(JWT_CONFIG), SharedModule, AdminModule],
-  controllers: [AuthenticationController, AdminController],
+  controllers: [AuthenticationController, AuxController],
   providers: [AuthenticationService, HashService, TokenService],
   exports: [AuthenticationService],
 })
